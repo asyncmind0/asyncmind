a827543e3787303d585873e363eb3542499b205c
Steven Joseph <steven@damagebdd.com>
2024-07-26 20:58:23 +1000

    Updated packages added trails swagger and other webstuf

--- src/asyncmind_app.erl
+++ src/asyncmind_app.erl
@@ -8,36 +8,61 @@
 -behaviour(application).
 
 -export([start/2, stop/1]).
+-export([start_phase/3]).
 
-start(_StartType, _StartArgs) ->
+-include_lib("kernel/include/logger.hrl").
+
+start(_StartType, _StartArgs) -> asyncmind_sup:start_link().
+
+get_trails() ->
+  Handlers = [erm_http, cowboy_swagger_handler],
+  Trails =
+    [
+      {"/", cowboy_static, {priv_file, asyncmind, "index.html"}},
+      {"/ws", websocket_handler, []},
+      {"/static/[...]", cowboy_static, {priv_dir, asyncmind, "static"}} | trails:trails(Handlers)
+    ],
+  trails:store(Trails),
+  trails:single_host_compile(Trails).
+
+
+start_phase(start_trails_http, _StartType, []) ->
   {ok, _} = application:ensure_all_started(gun),
   {ok, _} = application:ensure_all_started(erlexec),
   {ok, _} = application:ensure_all_started(ipfs),
   {ok, _} = application:ensure_all_started(erlcron),
   {ok, _} = application:ensure_all_started(gproc),
   exec:start(),
-  Dispatch =
-    cowboy_router:compile(
-      [
-        {
-          '_',
-          [
-            {"/", cowboy_static, {priv_file, asyncmind, "index.html"}},
-            {"/ws", websocket_handler, []},
-            {"/static/[...]", cowboy_static, {priv_dir, asyncmind, "static"}}
-          ]
-        }
-      ]
-    ),
+  Dispatch = get_trails(),
   {ok, WsPort} = application:get_env(asyncmind, ws_port),
-  {ok, _} = cowboy:start_clear(http, [{port, WsPort}], #{env => #{dispatch => Dispatch}}),
+  {ok, _} =
+    cowboy:start_clear(
+      http,
+      %[{ip, {0, 0, 0, 0}}, {port, WsPort}],
+      [{port, WsPort}],
+      #{
+        env => #{dispatch => Dispatch},
+        stream_handlers => [cowboy_telemetry_h, cowboy_metrics_h, cowboy_stream_h]
+      }
+    ),
+  ?LOG_INFO("Started cowboy.");
+
+start_phase(start_sync, _StartType, []) ->
+  logger:info("Starting sync."),
   case init:get_plain_arguments() of
-    [_, "shell"] -> sync:go();
-    _ -> ok
-  end,
-  asyncmind_sup:start_link().
+    [_, "shell"] ->
+      ?LOG_INFO("Sourc sync enabled.", []),
+      sync:go();
+
+    _ ->
+      ?LOG_INFO("Sourc sync disabled.", []),
+      ok
+  end.
 
 
-stop(_State) -> ok = cowboy:stop_listener(http).
+stop(_State) ->
+  ok = cowboy:stop_listener(http),
+  application:stop(gun),
+  ok.
 
 %% internal functions
